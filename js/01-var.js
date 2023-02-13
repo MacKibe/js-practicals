458cb9f408f27e520584b2cc0617e02c62579436
mackibe <ndichujames2030@gmail.com>
2023-02-13 11:32:08 +0300

    Forgot to add the other files

---
+++ js/01-var.js
@@ -0,0 +1,19 @@
+// challenge 1
+
+let firstname= "James"
+let lastname = "Kibe"
+let address = "main street"
+address = "first street"
+
+console.log(firstname ,lastname, address)
+
+// challenge 2
+
+let street = "Kise"
+let country = "Kenya"
+let fullmailingaddress = street + country
+
+console.log(fullmailingaddress);
+
+// chalenge 3
+
