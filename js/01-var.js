2262ee59be4bcc36b28a6868218df69713ead768
mackibe <ndichujames2030@gmail.com>
2023-02-14 11:30:05 +0300

    added 2 more practicals

--- js/01-var.js
+++ js/01-var.js
@@ -1,9 +1,10 @@
 // challenge 1
 
-let firstname= "James"
-let lastname = "Kibe"
-let address = "main street"
-address = "first street"
+const firstname= "James";
+const lastname = "Kibe";
+
+let address = "main street";
+address = "first street";
 
 console.log(firstname ,lastname, address)
 
@@ -11,9 +12,29 @@ console.log(firstname ,lastname, address)
 
 let street = "Kise"
 let country = "Kenya"
-let fullmailingaddress = street + country
+
+let fullmailingaddress = street + " " + country
 
 console.log(fullmailingaddress);
 
 // chalenge 3
+const score1 = 50;
+const score2 = 50;
+const score3 = 50;
+
+let total = score1 + score2 + score3;
+let average = total/3;
+
+console.log(total, average);
+
+const plates = 20;
+const people = 7;
+
+let platesRemaining = plates % people;
+
+platesRemaining++;
+
+const message = "There are " + platesRemaining + " plates available"
+console.log(message);
+
 
